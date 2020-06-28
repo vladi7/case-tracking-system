6a229c2a4ba04a341725cbc44621c7e02f04bc14
sabrinadjeddi <32584787+sabrinadjeddi@users.noreply.github.com>
2020-06-28 10:36:24 -0500

    Header and dashboard changed -Sabrina

---
+++ case-tracking-system/src/app/components/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {AuthService} from "../../shared/services/auth.service";
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+  constructor(public authService: AuthService,)
+  { }
+}
